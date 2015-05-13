b17c59fc4388b982c2263edd1094ab3b1d2b0f98
minamarjanovic <mina.marjanovic5@gmail.com>
2015-05-13 15:54:26 +0200

    Napravljeni paketi main i sistemske operacije u okviru test.

---
+++ test/main/FilmTest.java
@@ -0,0 +1,49 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FilmTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetOpis() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAdresaSlike() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNazivFilma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetReziser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetZanr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOcena() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/main/PomocnaProjekcijaTest.java
@@ -0,0 +1,34 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PomocnaProjekcijaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetNazivFilma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSala() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVreme() {
+		fail("Not yet implemented");
+	}
+
+}
